d7b18973c36f4c5c9f95af86a33f371011a8421a
Amrut <XKS9@eurofins.com>
2019-04-30 11:48:59 +0530

    Get Sources/

--- src/main/java/com/eurofins/amrut/springbootplay/service/NewsService.java
+++ src/main/java/com/eurofins/amrut/springbootplay/service/NewsService.java
@@ -1,10 +1,13 @@
 package com.eurofins.amrut.springbootplay.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,30 +17,44 @@ import org.springframework.web.client.RestTemplate;
 
 import com.eurofins.amrut.springbootplay.entity.Article;
 import com.eurofins.amrut.springbootplay.entity.Response;
+import com.eurofins.amrut.springbootplay.entity.Source;
 
 @RestController
 public class NewsService {
+	
+	@Autowired
+	NewsServiceImpl newsServiceImpl;
+	
 	static List<Article> articleList = null;
 	static List<Article> techArticleList = null;
 	static Map<String, List<Article>> cache = new HashMap<>();
-
+	static long lastRefreshTime = 0;
 	@Value("${news.api.key}")
 	private String newsApiKey;
 
 	@Value("${user.categories}")
 	private String userCategories;
+	
+	@Value("${news.refresh.interval}")
+	private double refreshInterval;
+	
+	@Value("${news.technology.categories}")
+	private String technologySources;
 
+	
+	
 	@RequestMapping(value = "/news/getLatest")
 	@ResponseBody
 	public List<Article> getLatestNews() {
-		final String uri = "https://newsapi.org/v2/top-headlines?language=en&country=in&apiKey=d7ae5b652f4d481d8cb7ded898d9d43f";
-
-		if (articleList != null)
-			return articleList;
-		else
+		final String uri = "https://newsapi.org/v2/top-headlines?language=en&apiKey="+newsApiKey+"&sources=bbc-news,cnn,business-insider,the-verge";
+		Date now = new Date();
+		long nowMs = now.getTime();
+		if (cache.get("headlines") == null || (nowMs - lastRefreshTime)> refreshInterval ) {
 			articleList = doRestCall(uri);
-		return articleList;
-
+			cache.put("headlines", articleList);
+			lastRefreshTime = nowMs;
+		}
+		return cache.get("headlines");
 	}
 
 	@RequestMapping("/news/source/{sourceName}")
@@ -45,7 +62,17 @@ public class NewsService {
 	public List<Article> getNewsFromSource(@PathVariable String sourceName) {
 		final String uri = "https://newsapi.org/v2/top-headlines?language=en&apiKey=" + newsApiKey + "&sources="
 				+ sourceName;
-		return doRestCall(uri);
+		if(sourceName==null) {
+			return null;
+		}
+		Date now = new Date();
+		long nowMs = now.getTime();
+		if (cache.get(sourceName) == null || (nowMs - lastRefreshTime)> refreshInterval ) {
+			articleList = doRestCall(uri);
+			cache.put(sourceName, articleList);
+			lastRefreshTime = nowMs;
+		}
+		return cache.get(sourceName);
 	}
 
 	@RequestMapping(value = "/news/search/{searchText}")
@@ -65,9 +92,16 @@ public class NewsService {
 	@RequestMapping(value = "/news/category/{category}")
 	@ResponseBody
 	public List<Article> getNewsForCategory(@PathVariable String category, boolean extendedSearch) {
-		final String uri = "https://newsapi.org/v2/top-headlines?language=en&country=in&apiKey=d7ae5b652f4d481d8cb7ded898d9d43f&category="
+		String country = "";
+		List<String> indianCategoryList = new ArrayList<>(Arrays.asList("politics","automobile","ipl"));
+		if(indianCategoryList.contains(category)) {
+			country = "in";
+		}
+		final String uri = "https://newsapi.org/v2/top-headlines?language=en&country=" + country + "&apiKey="+newsApiKey+"&category="
 				+ category;
-		if (cache.get(category) == null) {
+		Date now = new Date();
+		long nowMs = now.getTime();
+		if (cache.get(category) == null || (lastRefreshTime - nowMs)> refreshInterval ) {
 			List<Article> articles = doRestCall(uri);
 			if (articles.isEmpty()) {
 				articles = getLatestNewsForSearchText(category);
@@ -76,6 +110,7 @@ public class NewsService {
 				articles = getAllNewsForSearchText(category);
 			}
 			cache.put(category, articles);
+			lastRefreshTime = nowMs;
 		}
 		return cache.get(category);
 	}
@@ -83,26 +118,37 @@ public class NewsService {
 	@RequestMapping(value = "/news/myFeed")
 	@ResponseBody
 	public List<Article> getLatestFeed() {
-		final String uri = "https://newsapi.org/v2/top-headlines?language=en&country=in&apiKey=d7ae5b652f4d481d8cb7ded898d9d43f";
-		if (cache.get("myFeed") == null) {
+		Date now = new Date();
+		long nowMs = now.getTime();
+		final String uri = "https://newsapi.org/v2/top-headlines?language=en&country=us&apiKey=d7ae5b652f4d481d8cb7ded898d9d43f";
+		if (cache.get("myFeed") == null || (lastRefreshTime - nowMs)> refreshInterval ) {
 			List<Article> myFeed = new ArrayList<>();
 			for (String category : userCategories.split(",")) {
-				List<Article> temp = getNewsForCategory(category.split(":")[1],false);
-				if(!temp.isEmpty())
+				List<Article> temp = getNewsForCategory(category.split(":")[1], false);
+				if (!temp.isEmpty())
 					myFeed.add(temp.get(0));
 			}
 			cache.put("myFeed", myFeed);
+			Date lastRefreshTimedate = new  Date();
+			lastRefreshTimedate.setTime(lastRefreshTime);
+			System.out.println("Last refresh time: "+ lastRefreshTimedate);
+			lastRefreshTime = nowMs;
 		}
 		return cache.get("myFeed");
 
 	}
 
 	private List<Article> doRestCall(String uri) {
-		System.out.println("Doing rest call... for "+uri);
+		System.out.println("Doing rest call... for " + uri);
 		RestTemplate restTemplate = new RestTemplate();
 		Response result = restTemplate.getForObject(uri, Response.class);
-		System.out.println(result);
 		return result.getArticles();
 
+	}
+	
+	@RequestMapping(value = "/news/sources")
+	@ResponseBody
+	public List<Source> getNewsSources() {
+		return newsServiceImpl.getNewsSources();
 	}
 }
